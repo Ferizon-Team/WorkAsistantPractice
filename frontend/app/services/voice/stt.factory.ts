d602c9c70a410c27075f7986d04bb22cbae90203
Gopi-o <Aleshi.kriv@Gmail.com>
2026-06-16 15:04:51 +0400

    add http voiceMode and http url switch case

--- frontend/app/services/voice/stt.factory.ts
+++ frontend/app/services/voice/stt.factory.ts
@@ -1,8 +1,19 @@
 import type { SttService } from './stt.types'
 import { createStubSttService } from './stt.stub'
 import { createWebsocketSttService } from './stt.websocket'
+import { createHttpSttService } from './stt.http'
 
-export function createSttService(mode: 'stub' | 'websocket', wsUrl: string): SttService {
-    if (mode === 'websocket') return createWebsocketSttService(wsUrl)
-    return createStubSttService()
+export function createSttService(mode: 'stub' | 'websocket' | 'http', Url: string): SttService {
+    
+    switch (mode) {
+            case 'websocket':
+                return createWebsocketSttService(Url)
+                break;
+            case 'http':
+                return createHttpSttService(Url)
+                break;
+            default:
+                return createStubSttService()
+                break;
+        }
 }
